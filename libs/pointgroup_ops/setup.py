8df35fe654d98931b3a51254961956786053f59e
S. Nordin Abouzahra <NordinAbouzahra@gmail.com>
2025-07-15 21:10:28 -0400

    add Python environment for Windows
    * fixed VLA usage for MSVC in pointgroup_ops
    * added missing include and library paths
    * added CI testing for the Window YAML
    * pack and save environment as build artifact

--- libs/pointgroup_ops/setup.py
+++ libs/pointgroup_ops/setup.py
@@ -1,13 +1,17 @@
 import os
+import platform
+import sys
+import torch
 from sys import argv
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 from distutils.sysconfig import get_config_vars
 
 (opt,) = get_config_vars("OPT")
-os.environ["OPT"] = " ".join(
-    flag for flag in opt.split() if flag != "-Wstrict-prototypes"
-)
+if opt:
+    os.environ["OPT"] = " ".join(
+        flag for flag in opt.split() if flag != "-Wstrict-prototypes"
+    )
 
 
 def _argparse(pattern, argv, is_flag=True, is_list=False):
@@ -43,15 +47,29 @@ include_dirs = []
 if not (INCLUDE_DIRS is False):
     include_dirs += INCLUDE_DIRS
 
+if platform.system() == "Windows":
+    # Get the base path of the current Conda environment
+    base_path = sys.prefix
+
+    # Create the full path to the 'windows' include directory
+    include_windows_path = os.path.join(base_path, 'Library', 'include', 'windows')
+    include_dirs.append(include_windows_path) # sparsehash nonsense
+    conda_lib_path = os.path.join(sys.prefix, 'Library', 'lib')
+    print('='*20)
+    print(conda_lib_path)
+    torch_lib_dir = os.path.join(os.path.dirname(torch.__file__), 'lib')
+
 setup(
     name="pointgroup_ops",
     packages=["pointgroup_ops"],
     package_dir={"pointgroup_ops": "functions"},
+    install_requires=["torch"],
     ext_modules=[
         CUDAExtension(
             name="pointgroup_ops_cuda",
             sources=["src/bfs_cluster.cpp", "src/bfs_cluster_kernel.cu"],
-            extra_compile_args={"cxx": ["-g"], "nvcc": ["-O2"]},
+            library_dirs=[conda_lib_path, torch_lib_dir],
+            extra_compile_args={"cxx": ["-g"], "nvcc": ["-O2", "-gencode=arch=compute_75,code=sm_75",]},
         )
     ],
     include_dirs=[*include_dirs],
--- libs/pointops/setup.py
+++ libs/pointops/setup.py
@@ -1,12 +1,16 @@
 import os
+import platform
+import torch
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 from distutils.sysconfig import get_config_vars
 
 (opt,) = get_config_vars("OPT")
-os.environ["OPT"] = " ".join(
-    flag for flag in opt.split() if flag != "-Wstrict-prototypes"
-)
+
+if opt:
+    os.environ["OPT"] = " ".join(
+        flag for flag in opt.split() if flag != "-Wstrict-prototypes"
+    )
 
 src = "src"
 sources = [
@@ -16,18 +20,29 @@ sources = [
     if file.endswith(".cpp") or file.endswith(".cu")
 ]
 
+# Conditionally set the conda_lib_path for Windows
+conda_lib_path = None
+if platform.system() == "Windows":
+    conda_lib_path = os.path.join(sys.prefix, 'Library', 'lib')
+    torch_lib_dir = os.path.join(os.path.dirname(torch.__file__), 'lib')
+
+# Define the CUDA extension
+cuda_extension = CUDAExtension(
+    name="pointops._C",
+    sources=sources,
+    extra_compile_args={"cxx": ["-g"], "nvcc": ["-O2", "-gencode=arch=compute_75,code=sm_75",]},
+)
+
+# If conda_lib_path is set, add it to the library_dirs
+if conda_lib_path:
+    cuda_extension.library_dirs = [conda_lib_path, torch_lib_dir]
+
 setup(
     name="pointops",
     version="1.0",
     install_requires=["torch", "numpy"],
     packages=["pointops"],
     package_dir={"pointops": "functions"},
-    ext_modules=[
-        CUDAExtension(
-            name="pointops._C",
-            sources=sources,
-            extra_compile_args={"cxx": ["-g"], "nvcc": ["-O2"]},
-        )
-    ],
+    ext_modules=[cuda_extension],
     cmdclass={"build_ext": BuildExtension},
 )
